969badabe30ca09a5ec089b96ba71c4d625ef3b3
youssouf <nzomedia2@gmail.com>
2017-10-05 15:04:03 +0000

    Correction syntax async

--- commands.js
+++ commands.js
@@ -3,25 +3,31 @@ module.exports = {
 
     uninstall: {
         description: "Uninstall app and clean everything",
-        async handler(api) {
+        handler: function(api) {
             try {
                 const meteorSession = api.getSessions(["meteor"]);
                 const mongoSession = api.getSessions(["mongo"]);
 
                 //stop each service:
-                await api.runCommand("meteor.stop");
-                console.log("Meteor app and container stoped");
-                await api.runCommand("mongo.stop");
-                console.log("MongoDb server and container stoped");
-                await api.runCommand("mariadb.stop");
-                console.log("MariaDb server and container stoped");
+                api.runCommand("meteor.stop").then(function(){
+				console.log("Meteor app and container stoped");
+                		return api.runCommand("mongo.stop");
+				}).then(function(){
+                			console.log("MongoDb server and container stoped");
+                			return api.runCommand("mariadb.stop");
+				}).then(function(){
+				        console.log("MariaDb server and container stoped");
+				}).catch(function(error){
+					console.log("Erreur:", error);
+				});
+
                 
                 //delete Docker images
 
                 //delete scripts
 
                 //remove Docker ?
-                console.log("Fini");
+                //console.log("Fini");
             }
             catch(error) {
                 console.log("Mup error:", error);
